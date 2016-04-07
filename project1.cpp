9c62d9f64931750a0d1c9615201cd8f4b0d6fc8a
Chris Dower <devwowow@gmail.com>
2016-04-07 09:50:18 -0400

    fixed some spacing issues

--- project1.cpp
+++ project1.cpp
@@ -85,11 +85,11 @@ int main() {
       // DEBUG: Print ip address in binary
       /*
       for(int i=0; i<32; i++) {
-	if(addr & (1<<(31-i))) {
-	  cout << "1";
-	} else {
-	  cout << "0";
-	}
+	     if(addr & (1<<(31-i))) {
+	      cout << "1";
+	     } else {
+	      cout << "0";
+	     }
       }
       cout << endl;
       */
